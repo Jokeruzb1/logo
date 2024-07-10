79417d448da52e3006d87c970a8e52ad4803d6e0
Abuja'far <abujafarpardayev5@gmail.com>
2024-07-10 15:57:33 +0300

    add files

---
+++ main.js
@@ -0,0 +1,10 @@
+// let num1 = +prompt("1-son kiriting");
+// let num2 = +prompt("2-son kiriting");
+
+// if (num1 <num2) {
+//     for (let  i = i <= num1; i < num2; i++){
+//         document.write(i + " ");
+//     }
+// }
+
+
